56282f4ee794166392e24c271d404f0f09031067
Jun Ming Ooi <ojmjunming@gmail.com>
2022-03-09 05:17:49 +0400

    add card interaction and door opening

--- Assets/Scripts/CardReaderController.cs
+++ Assets/Scripts/CardReaderController.cs
@@ -5,14 +5,52 @@ using UnityEngine;
 public class CardReaderController : MonoBehaviour
 {
     public GameObject readerScreen;
+    public GameObject door;
+
+    public Vector3 doorStartPos;
+    public Vector3 doorEndPos = new Vector3(2.613f,-0.111f,4.212f);
+    public AudioSource beepSound;
+    public AudioSource doorSound;
+
+    float openDoorElapsedTime = 0.0f;
+    float openDoorDuration = 4.0f;
+    float lerpValue;
+    bool isFirstRun = true;
+
+    private void Start() {
+      doorStartPos = door.transform.position;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Card"))
         {
-            Material readerMaterial = readerScreen.GetComponent<Renderer>().material;
-
-            readerMaterial.color = new Color(33, 33, 33);
+            readerScreen.GetComponent<MeshRenderer>().material.color = new Color(0.2f, 1f, 0.2f);
+            
+            beepSound.Play();
+            StartCoroutine(OpenDoor());
         }
     }
+
+    IEnumerator OpenDoor()
+    {
+        if (isFirstRun) { // delays door opening by 2 seconds
+          yield return new WaitForSeconds(2);
+          isFirstRun = false;
+        }
+
+        doorSound.Play();
+        while (true)
+        {
+            if (openDoorElapsedTime < openDoorDuration)
+            {
+                door.transform.position = Vector3.MoveTowards(doorStartPos, doorEndPos, openDoorElapsedTime / openDoorDuration);
+                openDoorElapsedTime += Time.deltaTime;
+            } else if (openDoorElapsedTime > openDoorDuration) {
+              StopCoroutine(OpenDoor());
+            }
+            yield return null;
+        }
+    }
+
 }
