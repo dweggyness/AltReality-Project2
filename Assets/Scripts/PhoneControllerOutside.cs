df1bc32bacea5fb87d5ab3fe30ae04c3d23a6352
Jun Ming Ooi <colacolaojm@hotmail.com>
2022-03-10 01:21:35 +0400

    finished outside

---
+++ Assets/Scripts/PhoneControllerOutside.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneControllerOutside : MonoBehaviour
+{
+
+    bool lightActivated = false;
+    bool phoneRung = false;
+    public AudioSource phoneAudio;
+    public AudioSource phoneRingingAudio;
+    public float delay = 13.0f;
+
+    private void Awake()
+    {
+        this.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+    }
+
+    public void updatePulledAmount(float t)
+    {
+        if (t > 0.99 && !lightActivated)
+        {
+            startPhoneCall();
+        };
+    }
+
+
+    private void startPhoneCall()
+    {
+        lightActivated = true;
+        this.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+        phoneRingingAudio.Stop();
+        phoneAudio.Play();
+
+        StartCoroutine(turnLightOffAfterDelay());
+    }
+
+    public void ringPhone()
+    {
+        phoneRingingAudio.Play();
+    }
+
+    IEnumerator turnLightOffAfterDelay()
+    {
+        yield return new WaitForSeconds(delay);
+
+        this.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+    }
+}
